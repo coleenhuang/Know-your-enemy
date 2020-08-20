1c5a4bf105fe3e2f47080dc31c46539b8b14ee43
Coleen Huang <coleenhuang@gmail.com>
2020-08-20 16:21:12 -0700

    reorganized the cardList array

--- src/components/game/CardsContainer.js
+++ src/components/game/CardsContainer.js
@@ -4,11 +4,18 @@ import { connect } from 'react-redux';
 
 
 const CardsContainer = (props) => {
+    //FIXME:
+    //shuffle the array
+    //generate the cards 
+    
+
+    
+
     return (
         <div>
             {props.cardList.map(card => {
                 return (
-                    <Card key={card.id} />
+                    <Card key={card.id} back={card.back}/>
                 )
             })}
         </div>
--- src/reducers/cardList.js
+++ src/reducers/cardList.js
@@ -1,32 +1,32 @@
 export default () => [
     {   
         id: 1,
-        name: 'Dengue virus',
-        fact1: 'Transmitted by mosquitos'
+        back: 'Dengue virus',
+        type: 1
     },
-    {
+    {   
         id: 2,
-        name: 'Yellow fever virus',
-        fact1: 'Caused epidemics in New York during 1795, 1799 and 1803'
+        back: 'Causes a disease known as breakbone fever',
+        type: 1
     },
-    {
+    {   
         id: 3,
-        name: "Vibrio cholerae",
-        fact1: "Infects people through contaminated water sources"
+        back: 'Yellow fever virus',
+        type: 2
     },
-    {
+    {   
         id: 4,
-        name: "Poliovirus",
-        fact1: "Transmitted by the oral fecal route"
+        back: 'Caused epidemics in New York during 1795, 1799 and 1803',
+        type: 2
     },
     {
         id: 5,
-        name: "Toxoplasma gondii",
-        fact1: "causes toxoplasmosis"
+        back: "Hantavirus",
+        type: 3
     },
     {
-        id:6,
-        name: "Marburgvirus",
-        fact1: "Causes hemorragic fever"
+        id: 6,
+        back: "Spread by mice",
+        type: 3
     }
 ]
